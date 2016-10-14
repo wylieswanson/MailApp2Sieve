f16c9dfb9e013dc75be406d977b81d9428371049
Wylie Swanson <wylie@pingzero.net>
2016-10-14 10:39:31 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Command Line
+* sieve-filter -v -C -u wylie /var/mail/vmail/wylie/dovecot.sieve 'TrustCor/MsgSafe.io/Support'
+* sieve-filter -e -W -v -C -u wylie /var/mail/vmail/wylie/dovecot.sieve 'TrustCor/MsgSafe.io/Support'
+
+# Notes
+
+* https://mebsd.com/configure-freebsd-servers/dovecot-pigeonhole-sieve-filter-refilter-delivered-email.html
+* https://tty1.net/blog/2011/sieve-tutorial_en.html
+
---
+++ analyze.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import json
+import plistlib
+
+p = plistlib.readPlist('/Volumes/Storage/Users/wylie/Library/Mail/V3/MailData/SyncedRules.plist')
+
+
+stmt = 'if'
+def handle(header, expression, mbox):
+	global stmt
+	if header in ('To', 'Cc', 'From'):
+		print stmt, 'address :contains ["%s"]  "%s" { fileinto "INBOX.%s"; }' % (header, expression, mbox)
+	elif header == 'Subject':
+		print stmt, 'header :matches "Subject" ["*%s*"] { fileinto "INBOX.%s"; }' % (expression, mbox)
+	else:
+		raise ValueError(header)
+	stmt = 'elsif'
+
+j = json.dumps(p, sort_keys=True, indent=2, separators=(',', ': '))
+print j
+
+print len(p)
+
+print '''
+require ["fileinto", "reject"];
+'''
+
+"""
+for rule in p['rules']:
+	if 'Mailbox' not in rule: continue
+	mbox = rule['Mailbox'].split('/')[-1].split('.')[0]
+	print '#', rule['RuleName'], '->', mbox
+	for criteria in rule['Criteria']:
+		if criteria['Header'] == 'AnyRecipient':
+			handle('To', criteria['Expression'], mbox)
+			handle('Cc', criteria['Expression'], mbox)
+		else:
+			handle(criteria['Header'], criteria['Expression'], mbox)
+"""
+
+for rule in p:
+	if 'Mailbox' not in rule: continue
+	mbox = rule['Mailbox'].split('/')[-1].split(',')[0]
+	print "#", rule['RuleName'], '->', mbox
+	for criteria in rule['Criteria']:
+		if criteria['Header'] == 'AnyRecipient':
+			handle('To', criteria['Expression'], mbox)
+			handle('Cc', criteria['Expression'], mbox)
+		else:
+			handle(criteria['Header'], criteria['Expression'], mbox)
+
+
+print '''
+else {
+	keep;
+}
+'''
