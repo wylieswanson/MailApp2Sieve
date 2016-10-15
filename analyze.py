42c4ba1b3f0d893cd6dd278b5008d14890bd502f
Wylie Swanson <wylie@pingzero.net>
2016-10-14 17:24:58 -0700

    update for macOS sierra

--- analyze.py
+++ analyze.py
@@ -2,63 +2,66 @@
 
 import json
 import plistlib
+from urlparse import urlparse
+from urllib import unquote
 
 stmt = 'if'
 p = plistlib.readPlist(
-    '/Volumes/Storage/Users/wylie/Library/Mail/V3/MailData/SyncedRules.plist')
+    '/Volumes/Storage/Users/wylie/Library/Mail/V4/MailData/SyncedRules.plist')
 
 
-def handle(header, expression, mbox):
+def handle_any(header, expression, mbox):
     global stmt
     if header in ('To', 'Cc', 'From'):
         print stmt, \
-            'address :contains ["%s"]  "%s" { fileinto "INBOX.%s"; }' % (
+            'address :contains ["%s"]  "%s" {\n' \
+            '\tfileinto "%s";\n}' % (
                 header, expression, mbox)
     elif header == 'Subject':
         print stmt, \
-            'header :matches "Subject" ["*%s*"] { fileinto "INBOX.%s"; }' % (
+            'header :matches "Subject" ["*%s*"] {\n' \
+            '\tfileinto "%s";\n}' % (
                 expression, mbox)
     else:
         raise ValueError(header)
     stmt = 'elsif'
 
-j = json.dumps(p, sort_keys=True, indent=2, separators=(',', ': '))
-print j
 
-print len(p)
-
-print '''
-require ["fileinto", "reject"];
-'''
-
-"""
-for rule in p['rules']:
-    if 'Mailbox' not in rule: continue
-    mbox = rule['Mailbox'].split('/')[-1].split('.')[0]
-    print '#', rule['RuleName'], '->', mbox
+def handle_specific(criteria, mbox):
+    global stmt
+    print "elsif allof ("
     for criteria in rule['Criteria']:
-        if criteria['Header'] == 'AnyRecipient':
-            handle('To', criteria['Expression'], mbox)
-            handle('Cc', criteria['Expression'], mbox)
-        else:
-            handle(criteria['Header'], criteria['Expression'], mbox)
-            """
+        print '\taddress :contains ["%s"]  "%s",' % (
+            'To', criteria['Expression'])
+    print ') {\n\tfileinto "%s"\n}' % (mbox)
+
+
+j = json.dumps(p, sort_keys=True, indent=2, separators=(',', ': '))
+
+print 'require ["fileinto", "reject"];'
 
 for rule in p:
-    if 'Mailbox' not in rule:
+    if 'CopyToMailboxURL' not in rule:
         continue
-    mbox = rule['Mailbox'].split('/')[-1].split(',')[0]
-    print "#", rule['RuleName'], '->', mbox
-    for criteria in rule['Criteria']:
-        if criteria['Header'] == 'AnyRecipient':
-            handle('To', criteria['Expression'], mbox)
-            handle('Cc', criteria['Expression'], mbox)
-        else:
-            handle(criteria['Header'], criteria['Expression'], mbox)
+    mbox = unquote(
+        urlparse(rule['MailboxURL']).path).decode('utf8').strip('/')
+    print "\n#", rule['RuleName'], '\n#  ->', mbox
+    print "#  ", rule['AllCriteriaMustBeSatisfied']
+    if rule['AllCriteriaMustBeSatisfied'] is False:
+        for criteria in rule['Criteria']:
+            if 'AnyMessage' in criteria['Header']:
+                continue
+            if criteria['Header'] == 'AnyRecipient':
+                handle_any(
+                    'To', criteria['Expression'], mbox)
+                handle_any(
+                    'Cc', criteria['Expression'], mbox)
+            else:
+                handle_any(
+                    criteria['Header'], criteria['Expression'], mbox)
+    elif rule['AllCriteriaMustBeSatisfied'] is True:
+        handle_specific(rule['Criteria'], mbox)
 
+print "else {\nkeep;\n}"
 
-print '''
-else {
-keep;
-}
-'''
+# print j, len(p)
